810b3609386f2d3504c5dd6577be032a86c295b3
Rxqueen-ranju100 <115641789+Rxqueen-ranju100@users.noreply.github.com>
2022-11-04 22:18:05 +0530

    Add files via upload

---
+++ even or odd or prime R.py
@@ -0,0 +1,20 @@
+for x in range(1, 101):
+    if x % 2 == 0:
+         print(x)
+
+    for x in range(1, 101):
+        if x % 2 != 0:
+         print(x)
+
+for Number in range(1, 101):
+    count = 0
+    for i in range(2, (Number // 2 + 1)):
+        if Number % i == 0:
+            count = count + 1
+            break
+    if count == 0 and Number != 1:
+        print(" %d" % Number, end="  ")
+
+
+
+
---
+++ swap using temp R.py
@@ -0,0 +1,7 @@
+a=int(input('Enter the value of a: '))
+b=int(input('Enter the value of b: '))
+print("The numbers before swapping is: " +str(a) +'   '+str(b))
+temp = a
+a=b
+b=temp
+print("The numbers after swapping is: " +str(a) +'  '+str(b))
